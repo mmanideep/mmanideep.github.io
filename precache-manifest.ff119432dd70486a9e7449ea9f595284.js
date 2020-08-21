603850dd7958135f512fd980770d776df0fb8365
unknown <madabhushani.manideep2012@gmail.com>
2020-08-21 19:30:38 +0530

    Updated website with new design

---
+++ precache-manifest.ff119432dd70486a9e7449ea9f595284.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b2a1da63d82f9f4796379795bc6f6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0562e22cbcddeb4d849d",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "213290bae3442b4bfc71",
+    "url": "/static/css/main.2e60bfb4.chunk.css"
+  },
+  {
+    "revision": "0562e22cbcddeb4d849d",
+    "url": "/static/js/2.b76fd3f1.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.b76fd3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213290bae3442b4bfc71",
+    "url": "/static/js/main.b2503e44.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ec4ed188cbd2024488a4e86d25464137",
+    "url": "/static/media/Appreciations_Atmecs_Kudos.ec4ed188.jpg"
+  },
+  {
+    "revision": "ea37c50a76437310d8a234c580ec0801",
+    "url": "/static/media/Appreciations_NewsLetter.ea37c50a.jpg"
+  },
+  {
+    "revision": "a919730e0b4ee1ff2af8c75e2f3139be",
+    "url": "/static/media/Azure.a919730e.png"
+  },
+  {
+    "revision": "8f3f2453897eb7e5811403d63579eeaa",
+    "url": "/static/media/BGPicture_filter.8f3f2453.png"
+  },
+  {
+    "revision": "1651ac0d07bb9054124d93b16971d341",
+    "url": "/static/media/CSS3.1651ac0d.png"
+  },
+  {
+    "revision": "afa245f5f9bea9b5b8cd2237f3d71978",
+    "url": "/static/media/CSharp.afa245f5.svg"
+  },
+  {
+    "revision": "7f8d28b0302108886af3322458d13440",
+    "url": "/static/media/Docker.7f8d28b0.svg"
+  },
+  {
+    "revision": "6594b16f07c7c7491fe1336a1034c9dd",
+    "url": "/static/media/ETH-512.6594b16f.png"
+  },
+  {
+    "revision": "f546505b1f2cfe7f5de56588ffc07342",
+    "url": "/static/media/EmployeOfTheMonthNomination.f546505b.png"
+  },
+  {
+    "revision": "c02b7da93fea7f472b89583e23f12759",
+    "url": "/static/media/Git.c02b7da9.svg"
+  },
+  {
+    "revision": "2bf747f09a4f9135fbd3032432f4158b",
+    "url": "/static/media/Linux.2bf747f0.svg"
+  },
+  {
+    "revision": "1e83f7103d15e32357b2dc768106fac0",
+    "url": "/static/media/Postgresql.1e83f710.svg"
+  },
+  {
+    "revision": "3b84eb4026f0c704e7e3d16972bb0e3d",
+    "url": "/static/media/Python.3b84eb40.svg"
+  },
+  {
+    "revision": "e9f3d01c242e086f895bde131803b451",
+    "url": "/static/media/React.e9f3d01c.svg"
+  },
+  {
+    "revision": "425b82e778312aeed9ae1f18593cd04d",
+    "url": "/static/media/Redis.425b82e7.svg"
+  },
+  {
+    "revision": "3e576e4e61a7975e47dc80cd536a5a16",
+    "url": "/static/media/aboutBackground.3e576e4e.png"
+  },
+  {
+    "revision": "8590a2fa18e8515b689c9c4970fceb67",
+    "url": "/static/media/bg_compressed.8590a2fa.mp4"
+  },
+  {
+    "revision": "a93d65a39d6ced71208d19161ac8a700",
+    "url": "/static/media/contactBackground.a93d65a3.png"
+  },
+  {
+    "revision": "ee21c73a0c1cbfb3affc49bbc399d050",
+    "url": "/static/media/github-alt-brands.ee21c73a.svg"
+  },
+  {
+    "revision": "c69613c720b4874b76ecf1da384f5422",
+    "url": "/static/media/hackerrank-brands.c69613c7.svg"
+  },
+  {
+    "revision": "657a1973677c3afd56c621881f3ae048",
+    "url": "/static/media/linkedin-brands.657a1973.svg"
+  }
+]);
